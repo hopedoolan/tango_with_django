be7974df0d62ba2f74724cc804a8209e94200bfe
hopedoolan <2182667d@student.gla.ac.uk>
2017-01-27 13:00:29 +0000

    passes all tests

--- rango/models.py
+++ rango/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Category(models.Model) :
     name = models.CharField(max_length=128, unique=True)
-    view = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
 
